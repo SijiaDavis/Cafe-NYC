61ae3a2dbbed6435a53d36cea20ed6ff82c0dec6
SijiaDavis <arwen@ksu.edu>
2016-12-08 20:37:52 -0500

    More refactoring

--- spec/features/new_place_spec.rb
+++ spec/features/new_place_spec.rb
@@ -3,51 +3,24 @@ require "rails_helper"
 RSpec.feature "New place", :type => :feature do
   scenario "User types in a new place", :js => true do
     sign_in
-    visit "/places/new"
-
-    fill_in "place_name", :with => "Cafe Lingo"
-    fill_in "place_description", :with => "Where the cool kids are."
-    fill_in "place_address", :with => "68 Jay Street, Suite 720, Brooklyn 11201\t"
-    
-    find('#place_address').native.send_keys(:tab)
-    wait_for_ajax
-
+    fill_out_new_cafe_form
+    ajax_validation
     expect(page).to have_no_content('You already wrote about this place.')
   end
   
   scenario "User types in an existing place", :js => true do
     sign_in
-    visit "/places/new"
-
-    fill_in "place_name", :with => "Cafe Lingo"
-    fill_in "place_description", :with => "Where the cool kids are."
-    fill_in "place_address", :with => "68 Jay Street, Suite 720, Brooklyn 11201\t"
-    
+    fill_out_new_cafe_form
     click_button "Create"
-    
-    visit "/places/new"
-
-    fill_in "place_name", :with => "Cafe Lingo"
-    fill_in "place_description", :with => "Where the cool kids are."
-    fill_in "place_address", :with => "68 Jay Street, Suite 720, Brooklyn 11201\t"
-    
-    find('#place_address').native.send_keys(:tab)
-    wait_for_ajax
-
+    fill_out_new_cafe_form
+    ajax_validation
     expect(page).to have_content('You already wrote about this place.')
   end
   
   scenario "Another user types in an exisiting place created by some other user", :js => true do
     sign_in
-    visit "/places/new"
-
-    fill_in "place_name", :with => "Cafe Lingo"
-    fill_in "place_description", :with => "Where the cool kids are."
-    fill_in "place_address", :with => "68 Jay Street, Suite 720, Brooklyn 11201\t"
-    
-    find('#place_address').native.send_keys(:tab)
-    wait_for_ajax
-
+    fill_out_new_cafe_form  
+    ajax_validation
     expect(page).to have_no_content('You already wrote about this place.')
   end
 end
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -7,4 +7,16 @@ module FeatureHelpers
     fill_in "user_password", with: @user.password
     click_button "Log in"
   end
+  
+  def fill_out_new_cafe_form
+    visit "/places/new"
+    fill_in "place_name", :with => "Cafe Lingo"
+    fill_in "place_description", :with => "Where the cool kids are."
+    fill_in "place_address", :with => "68 Jay Street, Suite 720, Brooklyn 11201"  
+  end
+  
+  def ajax_validation
+    find('#place_address').native.send_keys(:tab)
+    wait_for_ajax
+  end
 end
